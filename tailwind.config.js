8195b957ddb450d1eccef7669eae0366d4f24e37
Sibabale <sibabale.joja@oldmutual.com>
2026-01-21 19:55:15 +0200

    style: added theming

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './App.tsx', './index.tsx', './components/**/*.{ts,tsx}', './**/*.{ts,tsx}'],
+  darkMode: 'class',
+  theme: {
+    fontFamily: {
+      sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      heading: ['Space Grotesk', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      mono: ['Noto Sans Mono', 'Menlo', 'Monaco', 'Courier New', 'monospace'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
